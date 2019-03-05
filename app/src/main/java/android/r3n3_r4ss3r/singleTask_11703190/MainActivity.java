de83ca28a0006834657b3b55d525dd064df30045
René <r1rasser@edu.aau.at>
2019-03-05 17:16:37 +0100

    TCP Sockets...
    created a socket which should connect to the server se2-isys.aau.at:53212...answer of the server will be displayed in the textview with placeholder 'Servers Answer'

--- app/src/main/java/android/r3n3_r4ss3r/singleTask_11703190/MainActivity.java
+++ app/src/main/java/android/r3n3_r4ss3r/singleTask_11703190/MainActivity.java
@@ -2,6 +2,15 @@ package android.r3n3_r4ss3r.singleTask_11703190;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view){
+        TextView serverAnswer = findViewById(R.id.serverAnswer);
+        String studentNumber = ((EditText)findViewById(R.id.student_num)).getText().toString();
+        if(!studentNumber.equals("")) {
+            try {
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+                DataOutputStream _COSI = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader _CISO = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                _COSI.writeBytes(studentNumber);
+                serverAnswer.setText(_CISO.readLine());
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            serverAnswer.setText(R.string.error_no_studentNum);
+        }
     }
 }
