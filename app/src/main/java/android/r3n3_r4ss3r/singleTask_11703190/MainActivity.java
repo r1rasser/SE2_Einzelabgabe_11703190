fd1fe62128c92fa7f99b3ff851bd657f6b234339
René <r1rasser@edu.aau.at>
2019-03-05 15:25:14 +0100

    Project created
    created android project in Android Studio and added all to git-repo.

---
+++ app/src/main/java/android/r3n3_r4ss3r/singleTask_11703190/MainActivity.java
@@ -0,0 +1,13 @@
+package android.r3n3_r4ss3r.singleTask_11703190;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
